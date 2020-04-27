943af834bcbc6dee82033069aebf071229a9765a
Suraj Bidnur <bidnur.suraj@gmail.com>
2020-04-27 21:17:38 +0530

    Some minor changes

--- src/final.py
+++ src/final.py
@@ -1,22 +1,24 @@
 import inference_test2 as inf
 import os
 import glob
-#i = 0
-src_path = os.getcwd()
-path = os.path.join(src_path[0:-4],"output","CLEANED_DATA")
 
-test_data_dir = os.path.join(path,"train")
+src_path = os.getcwd()
+# path = os.path.join(src_path[0:-4],"output","Infer_data")
+path = os.path.join(src_path[0:-4],"output","CLEANED_DATA","test")
+
+# test_data_dir = os.path.join(path,"test")
 
 test_img_ind = list()
-test_folders = os.listdir(test_data_dir)
+# test_folders = os.listdir(test_data_dir)
+# test_folders = os.listdir(path)
 x = 0
-for subdir, dirs, files in os.walk(test_data_dir):
-
-    for filename in files:
-        filepath = subdir + os.sep + filename
-        if filepath.endswith(".jpg"):
-            test_img_ind.append((x-1))
-    x = x + 1
+# for subdir, dirs, files in os.walk(test_data_dir):
+for subdir, dirs, files in os.walk(path):
+  for filename in files:
+    filepath = subdir + os.sep + filename
+    if filepath.endswith(".jpg"):
+      test_img_ind.append((x-1))
+  x = x + 1
 '''for i in range(len(test_folders)):
   if not (len(os.listdir(os.path.join(test_data_dir,test_folders[i]))) == 0):
     test_img_ind.append(i)'''
@@ -25,13 +27,13 @@ for subdir, dirs, files in os.walk(test_data_dir):
 print(test_img_ind)
 i=0
 count = 0
-for subdir, dirs, files in os.walk(test_data_dir):
-    for filename in files:
-        filepath = subdir + os.sep + filename
-
-        if filepath.endswith(".jpg"):
-            ind = inf.func(filepath,test_img_ind[i])
-            if(ind):
-                count = count + 1
-            i=i+1
+# for subdir, dirs, files in os.walk(test_data_dir):
+for subdir, dirs, files in os.walk(path):
+  for filename in files:
+    filepath = subdir + os.sep + filename
+    if filepath.endswith(".jpg"):
+      ind = inf.func(filepath,test_img_ind[i])
+      if(ind):
+        count = count + 1
+      i=i+1
 print('Accuracy is {} %'.format( (count/len(test_img_ind) )*100))
