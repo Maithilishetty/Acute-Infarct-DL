86097aeb531bd36c84f84da56f56472955a90ad8
Maithilishetty <35223503+Maithilishetty@users.noreply.github.com>
2020-04-30 13:45:50 +0530

    with augmentation

--- src/cnn_model.py
+++ src/cnn_model.py
@@ -73,22 +73,14 @@ def run_test_harness():
     # define model
     model = define_model()
     # create data generator
-    '''train_datagen = ImageDataGenerator(rescale=1.0 / 255.0,
-                             rotation_range=90,
-                             zoom_range=0.15,
-                             width_shift_range=0.2,
-                             height_shift_range=0.2,
-                             shear_range=0.15,
-                             horizontal_flip=True,
-                             fill_mode="nearest")'''
     datagen = ImageDataGenerator(rescale=1.0 / 255.0)
     # prepare iterators
     train_it = datagen.flow_from_directory(training_data_dir,
-                                           class_mode='categorical', batch_size=46, target_size=(200, 200),shuffle=False)
+                                           class_mode='categorical', batch_size=128, target_size=(200, 200),shuffle=False)
     test_it = datagen.flow_from_directory(test_data_dir,
-                                          class_mode='categorical', batch_size=46, target_size=(200, 200),shuffle=False)
+                                          class_mode='categorical', batch_size=128, target_size=(200, 200),shuffle=False)
     # fit model
-    history = model.fit_generator(train_it, steps_per_epoch=len(train_it), epochs=50, verbose=1)
+    history = model.fit_generator(train_it, steps_per_epoch=len(train_it), epochs=10, verbose=1)
     # evaluate model
     _, acc = model.evaluate_generator(test_it, steps=len(test_it), verbose=0)
     # k = model.predict(test_it)
@@ -109,7 +101,7 @@ def run_test_harness():
         k = category_list[i]
         print('Test Image {}: Belongs to class {}'.format(s,k))
         s = s+1
-    print('> %.9f' % (acc * 100.0))
+    print('> %.3f' % (acc * 100.0))
     # learning curves
     summarize_diagnostics(history)
 
